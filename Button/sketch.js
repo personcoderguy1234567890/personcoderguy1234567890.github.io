e1acb9ef9f1c774f7d80a0db296eb98a3175e23b
personcoderguy1234567890 <45461898+personcoderguy1234567890@users.noreply.github.com>
2019-03-13 15:08:54 -0600

    chess board

---
+++ Button/sketch.js
@@ -0,0 +1,25 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+function button() {
+  fill("red");
+  rect(windowWidth/2 - 100, windowHeight/2 - 50, 100, 50);
+}
+
+// function IsMousePressed() {
+//   if (mouseClicked() && mouseX = ) 
+
+// }
--- scene/sketch.js
+++ scene/sketch.js
@@ -1,16 +1,20 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
-
+// Ellipse gets drawn from the center
+// rectangle gets drawn from the top left corner
 function draw() {
   background(220);
-  ellipse(mouseX, mouseY, 50, 50);
+  fill("white");
+  rect(175, 100, 50,50);
+  fill("red");
+  rect(175, 150, 50, 100);
+  fill("black");
+  rect(225, 160, 50, 15);
+  fill("green");
+  rect(125, 160, 50, 15);
+  fill("purple");
+  rect(175, 250, 20, 50);
+  fill("orange");
+  rect(200, 250, 20, 50);
 }
