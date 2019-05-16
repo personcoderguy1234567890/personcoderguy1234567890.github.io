61d68c6d5e9d5ddd4005e6513c3ed9c07e626d8c
personcoderguy1234567890 <45461898+personcoderguy1234567890@users.noreply.github.com>
2019-05-16 15:16:00 -0600

    stuff

--- D-MajorProjectRedBall/sketch.js
+++ D-MajorProjectRedBall/sketch.js
@@ -5,6 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let x,y;
+let
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +14,33 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function showTile(location, x, y) {
+  if (location === "#") {
+    image(platform, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "C") {
+    image(coin, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "B") {
+    image(box, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "F") {
+    image(fly, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "P") {
+    image(p1, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else if (location === "S") {
+    image(slime, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+  else {
+    image(empty, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+  }
+}
+
+function ball() {
+
+
 }
