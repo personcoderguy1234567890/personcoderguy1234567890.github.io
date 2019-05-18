178bfd1bd0c1c5c685374128d6aecca2a0e289ce
personcoderguy1234567890 <45461898+personcoderguy1234567890@users.noreply.github.com>
2019-05-17 22:44:54 -0600

    made the ball move

--- D-MajorProjectRedBall/sketch.js
+++ D-MajorProjectRedBall/sketch.js
@@ -5,43 +5,51 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let x,y;
+let ballStuff;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+  function setup() {
+    createCanvas(400, 400);
+    ballStuff = {
+      x:50, 
+      y:350,
+      dx:5, 
+      dy:5,
+      size:50,
+      dz:dx**2 + dy**2,
+      };
+  }
 
-function draw() {
-  background(220);
-  ball();
-}
+  function draw() {
+    background(220);
+    base();
+    move();
+    ball();
+  }
 
-// function showTile(location, x, y) {
-//   if (location === "#") {
-//     image(platform, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-//   }
-//   else if (location === "C") {
-//     image(coin, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-//   }
-//   else if (location === "B") {
-//     image(box, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-//   }
-//   else if (location === "F") {
-//     image(fly, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-//   }
-//   else if (location === "P") {
-//     image(p1, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-//   }
-//   else if (location === "S") {
-//     image(slime, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-//   }
-//   else {
-//     image(empty, x * tileWidth, y * tileHeight, tileWidth, tileHeight);
-//   }
-// }
+  function ball() {
+    fill("red");
+    ellipse(ballStuff.x, ballStuff.y, ballStuff.size, ballStuff.size);
+  }
 
-function ball() {
-  fill("red");
-  ellipse(width/2, height/2, 400, 400);
+  function move() {
+    if (keyIsPressed && (key === "d")) {
+      ballStuff.x += ballStuff.dx;
+    }
+    if (keyIsPressed && (key === "a")) {
+      ballStuff.x -= ballStuff.dx;
+    }
+    if (keyIsPressed && (key === "w")) {
+      ballStuff.y -= ballStuff.dy;
+    }
+    if (keyIsPressed && (key === "d" && key === "w")) {
+      ballStuff.x += dz;
 
-}
+    }
+  }
+
+  function base() {
+    fill("black");
+    rect(0, 350, 400, 50);
+
+
+  }
